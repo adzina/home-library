d83a6918c13045ee3938511ecf7b615fb9c094ec
adzina <adzina3@vp.pl>
2018-05-14 11:00:29 +0200

    user sees collections in library view

--- assets/app/components/libraries/libraries.component.ts
+++ assets/app/components/libraries/libraries.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend.service';
 import { Router } from '@angular/router';
+import { Collection } from '../../models/collection';
 @Component({
   selector: 'app-libraries',
   templateUrl: './libraries.component.html'
 })
 
 export class LibrariesComponent implements OnInit {
+  private collections: Collection[];
   constructor(private _backendService: BackendService,
               private _router: Router) { }
 
 
   ngOnInit() {
+    this._backendService.getMyCollections(localStorage.getItem("userID")).subscribe(data=>{
+      this.collections = data;
+    })
   }
 
 

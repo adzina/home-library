92e530ee7a6d022aed16824353e16a2284ec489a
adzina <adzina3@vp.pl>
2018-06-07 21:35:40 +0200

    book duplicates cannot be added

--- api/controllers/BookController.ts
+++ api/controllers/BookController.ts
@@ -2,18 +2,28 @@ declare var sails: any;
 
 module.exports = {
   create: function(req,res){
-    return sails.models.book.create({collectionID: req.param("collectionID"),
+    sails.models.book.findOne({collectionID: req.param("collectionID"),
     title: req.param("title"), author: req.param("author"), year: req.param("year"),
-    pages:req.param("pages")})
-    .exec(function (err:any, book:any){
-       if (err) {
-         sails.log.debug("Error in: creating book");
-         sails.log.error(err);
-         return res.serverError(err); }
+    pages:req.param("pages")}).exec(function(err:any, book:any){
+      console.log(book)
+      if(book!=undefined)
+        return res.serverError("This book already is in the collection");
+      else{
+        return sails.models.book.create({collectionID: req.param("collectionID"),
+        title: req.param("title"), author: req.param("author"), year: req.param("year"),
+        pages:req.param("pages")})
+        .exec(function (err:any, book:any){
+           if (err) {
+             sails.log.debug("Error in: creating book");
+             sails.log.error(err);
+             return res.serverError(err); }
 
-       sails.log.debug("Book created");
-       return res.json(book.id);
+           sails.log.debug("Book created");
+           return res.json(book.id);
 
-     });
+         });
+      }
+    })
+
   }
 };
